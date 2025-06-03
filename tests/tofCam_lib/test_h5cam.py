bb5e5a97af53416449d75d2bf3d773df2fde17b6
Ugurcan Cakal <ugurcan.cakal@gmail.com>
2025-06-03 10:01:57 +0200

    Add the point cloud test too

--- tests/tofCam_lib/test_h5cam.py
+++ tests/tofCam_lib/test_h5cam.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_h5cam():
+def test_h5cam_amplitude():
     from pathlib import Path
 
     import numpy as np
@@ -24,4 +24,31 @@ def test_h5cam():
     _frame = cam.get_amplitude_image()
     assert isinstance(_frame, np.ndarray)
     assert len(_frame) > 0
+    assert __index == cam.index - 1
+
+
+def test_h5cam_point_cloud():
+    from pathlib import Path
+
+    import numpy as np
+
+    from epc.tofCam_lib.h5Cam import H5Cam
+    source_path = Path(__file__).parent.parent / "data" / "point_cloud_w544c005_250527_134722.h5"
+    cam = H5Cam(source=source_path, continuous=True)
+    assert len(cam) > 0
+    __index = cam.index
+
+    with pytest.raises(ValueError):
+        cam.get_distance_image()
+    with pytest.raises(ValueError):
+        cam.get_raw_dcs_images()
+    with pytest.raises(ValueError):
+        cam.get_amplitude_image()
+    with pytest.raises(ValueError):
+        cam.get_grayscale_image()
+
+    _pc, _amplitude = cam.get_point_cloud()
+    assert isinstance(_pc, np.ndarray)
+    assert isinstance(_amplitude, np.ndarray)
+    assert _pc.shape[1] == _amplitude.shape[0]
     assert __index == cam.index - 1
