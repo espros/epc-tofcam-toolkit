60852774c30f429dacdba33af6dfae6c2aa02056
Nilluka Piyasinghe <npi@espros.com>
2026-01-28 11:13:12 +0000

    Corrected lens calibration data for p101152 and p101156 (Real ImageHeight...
    Corrected lens calibration data for p101152 and p101156 (Real ImageHeight instead of Paraxial Image Height).

--- src/epc/tofCam_gui/config.py
+++ src/epc/tofCam_gui/config.py
@@ -8,21 +8,32 @@ CrcCalc_linux = TOFCAM_LIB_BIN / "CrcCalc_linux.so"
 CrcCalc = TOFCAM_LIB_BIN / "CrcCalc.dll"
 CrcCalc_darwin = TOFCAM_LIB_BIN / "CrcCalc_darwin.a"
 
-WIDE_FIELD = DATA/"lense_calibration_wide_field.csv"
-NARROW_FIELD = DATA/"lense_calibration_narrow_field.csv"
-STANDARD_FIELD = DATA/"lense_calibration_standard_field.csv"
+NARROW_FIELD     = DATA/"lens_calibration_narrow_field_p100596.csv"
+STANDARD_FIELD   = DATA/"lens_calibration_standard_field_p100843.csv"
+WIDE_FIELD       = DATA/"lens_calibration_wide_field_p100506.csv"
+WIDE_WIDE_FIELD  = DATA/"lens_calibration_wide_wide_field_p101152.csv"
+ULTRA_WIDE_FIELD = DATA/"lens_calibration_ultra_wide_field_p101156.csv"
 
 
 if __name__ == "__main__":
-    with open(WIDE_FIELD, "r") as f:
-        x = f.read()
-        print(x)
 
     with open(NARROW_FIELD, "r") as f:
         x = f.read()
         print(x)
 
     with open(STANDARD_FIELD, "r") as f:
+        x = f.read()
+        print(x)
+
+    with open(WIDE_FIELD, "r") as f:
+        x = f.read()
+        print(x)
+
+    with open(WIDE_WIDE_FIELD, "r") as f:
+        x = f.read()
+        print(x)
+
+    with open(ULTRA_WIDE_FIELD, "r") as f:
         x = f.read()
         print(x)
 
